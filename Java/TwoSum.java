cb73ea731129bf2394d857b50c5f35420afbc1e4
Vonzpf <eternityzpf@sina.com>
2017-03-26 23:24:43 +0800

    1.Two Sum

--- Java/TwoSum.java
+++ Java/TwoSum.java
@@ -4,6 +4,17 @@ package LeetCode.Java;
 /**
  * Created by Vonzpf on 17/3/20.
  */
+/**-------------------------Title---------------------------------
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] Indice = new int[2];
