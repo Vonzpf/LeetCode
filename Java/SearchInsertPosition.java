d9b101bdfb10f303beb2e12cee2513986081e51c
Vonzpf <eternityzpf@sina.com>
2017-04-14 21:02:56 +0800

    35.Search Insert Position

--- Java/SearchInsertPosition.java
+++ Java/SearchInsertPosition.java
@@ -1,7 +1,34 @@
 package LeetCode.Java;
 
 /**
- * Created by tank on 17/4/14.
+ * Created by Vonzpf on 17/4/14.
+ */
+/**-------------------------Title---------------------------------
+ Given a sorted array and a target value, return the index if the target is found.
+ If not, return the index where it would be if it were inserted in order.
+
+ You may assume no duplicates in the array.
+
+ Here are few examples.
+ [1,3,5,6], 5 → 2
+ [1,3,5,6], 2 → 1
+ [1,3,5,6], 7 → 4
+ [1,3,5,6], 0 → 0
  */
 public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int indice = 0;
+        for (int i = 0; i < nums.length; i ++){
+            if (target <= nums[i]){
+                break;
+            }
+            else {
+                indice += 1;
+            }
+            if (indice == nums.length - 1){
+                return nums.length;
+            }
+        }
+        return indice;
+    }
 }
