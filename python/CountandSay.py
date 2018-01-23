bc933ad82762a0129efa2164270b2412a6bbbcc3
Vonzpf <eternityzpf@sina.com>
2018-01-23 16:05:20 +0800

    38. Count and Say

---
+++ python/CountandSay.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+# @Author zpf
+import re
+'''
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string.
+'''
+
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = '1'
+        for _ in range(n - 1):
+            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        return s
+
+
+if __name__ == "__main__":
+    sample = Solution()
+    print(sample.countAndSay(5))
