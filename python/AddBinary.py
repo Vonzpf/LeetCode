a9ee0bf3b9804ca22738e26d68e0f224634bcd13
Vonzpf <eternityzpf@sina.com>
2018-01-23 16:38:11 +0800

    67.Add Binary

---
+++ python/AddBinary.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+# @Author zpf
+'''
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+'''
+
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a, 2) + int(b, 2)).replace("0b", '')
+
+if __name__ == "__main__":
+    sample = Solution()
+    print(sample.addBinary('11', '1'))
