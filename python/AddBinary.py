c39de0f80094c581a13c56b5a491336213661ae9
Vonzpf <eternityzpf@sina.com>
2018-01-24 16:40:39 +0800

    67.Add Binary

--- python/AddBinary.py
+++ python/AddBinary.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
 
 # @Author zpf
-'''
+"""
 Given two binary strings, return their sum (also a binary string).
 
 For example,
 a = "11"
 b = "1"
 Return "100".
-'''
+"""
 
 
 class Solution(object):
