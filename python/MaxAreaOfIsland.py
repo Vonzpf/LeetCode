ddf217f5ef5730dcab4093325c60979b82d0eb13
Vonzpf <eternityzpf@sina.com>
2018-04-17 16:35:46 +0800

    695. Max Area of Island

---
+++ python/MaxAreaOfIsland.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+# @Author zpf
+"""
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land)
+connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array.
+(If there is no island, the maximum area is 0.)
+"""
+
+
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if 0 <= i < m and 0 <= j < n and grid[i][j]:
+                grid[i][j] = 0
+                return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
+            return 0
+
+        areas = [dfs(i, j) for i in range(m) for j in range(n) if grid[i][j]]
+        return max(areas) if areas else 0
+
+
+if __name__ == "__main__":
+    sample = Solution()
+    print(sample.maxAreaOfIsland([[0, 0, 0]]))
