75fce4014d86166602679ace52ead57a130494ae
Vonzpf <eternityzpf@sina.com>
2018-04-17 16:59:09 +0800

    771. Jewels and Stones

---
+++ python/JewelsAndStones.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+# @Author zpf
+"""
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: J = "z", S = "ZZ"
+Output: 0
+
+Note:
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+"""
+
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        x = 0
+        for e in list(S):
+            if e in set(J):
+                x += 1
+        return x
+
+
+if __name__ == "__main__":
+    sample = Solution()
+    print(sample.numJewelsInStones(J="aA", S="aAAbbbb"))
